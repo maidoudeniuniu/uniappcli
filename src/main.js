5aee1d93ed7ab2530aee77b108fc4ed588436d51
aniu <767159519@qq.com>
2021-08-17 01:53:05 +0800

    添加数据

--- src/main.js
+++ src/main.js
@@ -1,13 +1,29 @@
+import Vue from 'vue'
 import App from './App'
 import store from './store'
-
-
-import Vue from 'vue'
-
+import bootstrap from '@/core/bootstrap.js'
+import '@/router'
 
 // 引入全局uView
 import uView from '@/uview-ui'
 Vue.use(uView)
+
+import cmRoute from '@/utils/cmRoute.js'
+import getVlidate from '@/utils/http.js'
+
+
+Vue.prototype.$cmRoute = cmRoute
+Vue.prototype.$getVlidate = getVlidate
+
+import {
+	storeApi,
+	mpApi,
+	shopApi
+} from '@/api'
+
+Vue.prototype.$storeApi = storeApi
+Vue.prototype.$mpApi = mpApi
+Vue.prototype.$shopApi = shopApi
 
 
 Vue.config.productionTip = false
@@ -24,7 +40,8 @@ App.mpType = 'app'
 
 const app = new Vue({
 	store,
-	...App
+	...App,
+	created: bootstrap
 })
 app.$mount()
 
